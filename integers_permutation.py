3e0810a403d985b24db630acdd9de4452691f1df
IA <44641020+aiPhD@users.noreply.github.com>
2018-11-27 15:16:57 +0100

    Create integers_permutation.py

---
+++ integers_permutation.py
@@ -0,0 +1,15 @@
+# Calling the requested package and library
+from itertools import permutations 
+
+# Defining permutations array of integers
+perm = permutations([3, 6, 9]) 
+
+# Requesting the program output
+for (i) in list(perm): 
+  print (i) 
+
+# Full program
+from itertools import permutations
+perm = permutations([3, 6, 9])    
+for (i) in list(perm):
+  print (i)
